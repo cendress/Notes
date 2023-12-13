ed15b8e7b296aff21a56415405286f395a39c819
cendress <95492688+cendress@users.noreply.github.com>
2023-12-13 09:45:58 -0500

    Add DetailVC

---
+++ Notes/DetailVC.swift
@@ -0,0 +1,17 @@
+//
+//  DetailVC.swift
+//  Notes
+//
+//  Created by Christopher Endress on 12/13/23.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+}
