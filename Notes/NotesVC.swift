b67d94af66531b5fae4c01408a804126a91f2ebc
cendress <95492688+cendress@users.noreply.github.com>
2023-12-13 09:23:58 -0500

    Change tint color

--- Notes/NotesVC.swift
+++ Notes/NotesVC.swift
@@ -12,7 +12,28 @@ class NotesVC: UITableViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+ 
+  }
+  
+  //MARK: - Tableview data source methods
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    notes.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "NoteCell", for: indexPath)
+    let name = notes[indexPath.row].title
+    cell.textLabel?.text = name
+    cell.accessoryType = .disclosureIndicator
+    return cell
+  }
+  
+  //MARK: - Tableview delegate methods
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    //push detailVC
   }
 }
 
