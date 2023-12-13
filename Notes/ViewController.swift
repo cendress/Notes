9d33f3f62834eb11cdd1f00ca406074a3baf9259
cendress <95492688+cendress@users.noreply.github.com>
2023-12-13 09:04:44 -0500

    Plan code

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
   }
-
-
 }
+
+//create a notes data model
+//configure a tableview
+//add a detailvc with a textview
+//push detailvc from tableview
+//save the data with userdefaults
+//add a activityviewcontroller to detailvc
+//add delete and compose tool bar button items to detailvc
+//change UI to look like the Notes app
 
